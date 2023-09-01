39f3a6217d3b3a006f959a2737b501590cc39545
szmcdull <machine.gw@gmail.com>
2023-09-01 21:36:10 +0800

    gmap.Values

--- gmap/gmap.go
+++ gmap/gmap.go
@@ -26,6 +26,14 @@ func Keys[K comparable, V any](m map[K]V) []K {
 	return result
 }
 
+func Values[K comparable, V any](m map[K]V) []V {
+	result := make([]V, 0, len(m))
+	for _, v := range m {
+		result = append(result, v)
+	}
+	return result
+}
+
 func SortedKeys[K constraints.Ordered, V any](m map[K]V) []K {
 	keys := Keys(m)
 	garray.Sort(keys)
